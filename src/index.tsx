1370fc6b6f02a3e770cc19c3361b09e6ca2f188a
weihongliang233 <weijohn158@gmail.com>
2023-08-26 13:28:42 +0800

    在 react 中工作

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 import * as mapboxgl from 'mapbox-gl';
 //import './mapbox-gl.css';
 
 const App = () => {
   return <>
-    <div>nothisng</div>
+    <div id='map' style={{
+      height: 1000,
+      width: 2000
+    }}></div>
+    <ChildComp></ChildComp>
   </>;
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const ChildComp = ()=> {
+  useEffect(()=>{
+    mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94LWdsLWpzIiwiYSI6ImNram9ybGI1ajExYjQyeGxlemppb2pwYjIifQ.LGy5UGNIsXUZdYMvfYRiAQ'
+    new mapboxgl.Map({
+        container: 'map',
+        zoom: 12.5,
+        center: [-122.4194, 37.7749],
+        hash: true,
+    });        
+  }, [])
+  return <>
+  </>
+}
 
 
-
-
-mapboxgl.accessToken = 'pk.eyJ1IjoibWFwYm94LWdsLWpzIiwiYSI6ImNram9ybGI1ajExYjQyeGxlemppb2pwYjIifQ.LGy5UGNIsXUZdYMvfYRiAQ'
-new mapboxgl.Map({
-    container: 'A',
-    zoom: 12.5,
-    center: [-122.4194, 37.7749],
-    hash: true,
-});
+createRoot(document.getElementById('root')).render(<App></App>);
