1055893608b6c26c0910bc43eaefd067b98f5e03
weihongliang233 <52309635+weihongliang233@users.noreply.github.com>
2024-03-29 09:27:04 +0000

    .

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 unzip -jo ./resources/${DATASET}.zip -d ./dist/${DATASET}
-unzip ./resources/${DATASET}.zip -d ./3dcitydb-client/${DATASET}
+unzip ./resources/${DATASET}.zip -d ./3dcitydb-client/
--- scripts/onCreate.sh
+++ scripts/onCreate.sh
@@ -3,4 +3,4 @@ pnpm install --ignore-scripts && pnpm husky install
 # 填入你的 private_key
 cat scripts/env.sh >> ~/.bashrc
 
-pip install geopy
+pip install geopy flask flask-cors
